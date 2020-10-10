ea9a08e7dff31c8b441d03b996ce2e761a774e52
panic-coder <androidshubham@gmail.com>
2020-10-10 19:59:19 +0530

    updated code

--- employeeWage.sh
+++ employeeWage.sh
@@ -1,7 +1,6 @@
 #Welcome to employee wage computation
 
 IS_PART_TIME=1;
-IS_FULL_TIME=2;
 EMP_RATE_PER_HR=20;
 
 function getWorkHrs() {
@@ -9,15 +8,12 @@ function getWorkHrs() {
 		$IS_FULL_TIME)
 			empHrs=8
 			;;
-		$IS_PART_TIME)
-			empHrs=4
-			;;
 		*)
 			empHrs=0
 			;;
 	esac
 }
 
-empCheck=$((RANDOM%3))
+empCheck=$((RANDOM%2))
 dailyWages=$(($empHrs*$EMP_RATE_PER_HR))
 
